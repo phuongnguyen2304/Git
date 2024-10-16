69ac6a3e03ef5db80825dfb3dc9604f79ed8c5e9
phuongnguyen2304 <nguyenphuong23704@gmail.com>
2024-10-16 08:03:52 +0700

    update

--- src/main/java/com/example/git/HelloApplication.java
+++ src/main/java/com/example/git/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("hi");
+        System.out.println("aaaa");
     }
 
     public static void main(String[] args) {
